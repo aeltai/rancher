a0b4e4f3d0769fa409d63d45d55f0b63409ff3cf
aeltai <ala.eltai@suse.com>
2026-07-24 23:45:55 +0200

    Revert "Add dedicated IPv4/IPv6 fields to /v3/nodes API"
    This reverts commit f03a5b86099b548a599ee1783dfe6489c15845a1.

--- pkg/schemas/mapper/node_address.go
+++ pkg/schemas/mapper/node_address.go
@@ -1,19 +1,12 @@
 package mapper
 
 import (
-	"net/netip"
-
 	"github.com/rancher/norman/types"
-	"github.com/rancher/norman/types/convert"
 	"github.com/rancher/norman/types/values"
 )
 
 const (
-	extIPField   = "externalIpAddress"
-	ipv4Field    = "ipv4Address"
-	ipv6Field    = "ipv6Address"
-	extIPv4Field = "externalIpv4Address"
-	extIPv6Field = "externalIpv6Address"
+	extIPField = "externalIpAddress"
 )
 
 type NodeAddressMapper struct {
@@ -22,41 +15,14 @@ type NodeAddressMapper struct {
 func (n NodeAddressMapper) FromInternal(data map[string]interface{}) {
 	addresses, _ := values.GetSlice(data, "addresses")
 	for _, address := range addresses {
-		t := convert.ToString(address["type"])
-		a := convert.ToString(address["address"])
-		if a == "" {
-			continue
-		}
-
-		switch t {
-		case "InternalIP":
+		t := address["type"]
+		a := address["address"]
+		if t == "InternalIP" {
 			data["ipAddress"] = a
-			setFamilyAddress(data, a, ipv4Field, ipv6Field)
-		case "ExternalIP":
+		} else if t == "ExternalIP" {
 			data[extIPField] = a
-			setFamilyAddress(data, a, extIPv4Field, extIPv6Field)
-		case "Hostname":
+		} else if t == "Hostname" {
 			data["hostname"] = a
-		}
-	}
-}
-
-// setFamilyAddress records the first IPv4 and IPv6 address seen for the given fields.
-// Existing ipAddress/externalIpAddress remain last-wins for backward compatibility.
-func setFamilyAddress(data map[string]interface{}, address, v4Field, v6Field string) {
-	addr, err := netip.ParseAddr(address)
-	if err != nil {
-		return
-	}
-	if addr.Is4() || addr.Is4In6() {
-		if _, exists := data[v4Field]; !exists {
-			data[v4Field] = addr.Unmap().String()
-		}
-		return
-	}
-	if addr.Is6() {
-		if _, exists := data[v6Field]; !exists {
-			data[v6Field] = addr.String()
 		}
 	}
 }
